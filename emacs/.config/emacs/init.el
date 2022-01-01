4209da1d8aa0c33df5f5de11ad6eef1125c931dc
Tibor Pilz <tibor@pilz.berlin>
2022-01-01 09:04:04 +0100

    Add emacs config

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,40 @@
+; Install Packages
+(require 'package)
+ 
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-list
+      '(evil evil-collection org magit neotree))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(setq evil-want-keybinding nil)
+(require 'evil)
+(evil-mode t)
+(evil-collection-init)
+
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(display-line-numbers-mode)
+(setq display-line-numbers 'relative)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil-collection neotree magit evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
